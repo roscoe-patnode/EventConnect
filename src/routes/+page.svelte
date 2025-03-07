45f7bea569e3453ad709330b7760e9be9e7d5e04
Noah <146378130+Rybose@users.noreply.github.com>
2025-03-07 14:53:25 -0800

    Updated landing page with flavor text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<div class="max-w-5xl mx-auto">
-    <div class=" h-full w-full text-zinc-800 text-2xl "> LANDING PAGE</div>
+<div class="min-h-screen flex flex-col justify-between items-center">
+    <div class="max-w-5xl mx-auto text-zinc-800 text-center">
+        <!-- Main Heading -->
+        <h1 class="text-4xl font-bold mb-4">Welcome to the VTMO Event Planner App</h1>
+
+        <!-- Subheading -->
+        <p class="text-xl">Please Login/Signup to proceed</p>
+    </div>
 </div>
